5030eec514ba977c4ec334ea5adb807053806997
vestick <xorayesno@gmail.com>
2022-09-04 15:56:00 +0300

    Initial commit

---
+++ src/test/java/com/example/app/RestUserH2/RestUserH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.RestUserH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestUserH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
